56d3427bdc04af4a3409c5f58acea2f8b4749c7a
RedYou <jcdemers123@gmail.com>
2023-05-14 22:43:02 -0400

    no message

--- testNewCompiler/Class.cs
+++ testNewCompiler/Class.cs
@@ -8,9 +8,12 @@ namespace RedRust
 {
     internal class Class
     {
+        private bool included = false;
+
         public readonly string Name;
         public readonly Class? Extends;
         public readonly Dictionary<string, Class> Variables = new();
+        public readonly Dictionary<string, Class> Functions = new();
 
         public Class(string name, Class? extends)
         {
@@ -24,6 +27,18 @@ namespace RedRust
             if (!r && Extends is not null)
                 r = Extends.GetVar(name, out var);
             return r;
+        }
+
+        public void Compile()
+        {
+            if (included)
+                return;
+            included = true;
+
+            Extends?.Compile();
+
+            StreamWriter sw = Compiler.Instance.StreamWriter;
+
         }
     }
 }
---
+++ testNewCompiler/Compiler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedRust
+{
+    internal class Compiler
+    {
+        public readonly StreamWriter StreamWriter;
+
+        private Compiler(string file)
+        {
+            StreamWriter = File.CreateText(file);
+        }
+
+
+        private static Compiler? instance;
+        public static Compiler Instance
+        {
+            get
+            {
+                if (instance is null)
+                    throw new Exception();
+                return instance;
+            }
+        }
+
+        public static Compiler GetInstance(string file)
+        {
+            if (instance is null)
+                instance = new(file);
+            else
+                throw new Exception();
+            return instance;
+        }
+    }
+}
